0a2d39642df56be89aa127ec61bcb07a18517361
ESSutherland <essutherland1@dmacc.edu>
2017-11-30 16:36:01 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimeSelectComponent } from './time-select/time-select.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeSelectComponent
   ],
   imports: [
     BrowserModule
